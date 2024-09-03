14242234e1f3617932618c6b10bd7e9b837b1318
Thullyoo <thullyofilho@gmail.com>
2024-09-03 11:22:49 -0300

    feat: criação do repository para users

---
+++ src/main/java/br/thullyo/springsecurity6jwt/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.thullyo.springsecurity6jwt.repository;
+
+import br.thullyo.springsecurity6jwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
