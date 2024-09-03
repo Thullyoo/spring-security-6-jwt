699135cdefff06927aba5dd789a999836ecdb029
Thullyoo <thullyofilho@gmail.com>
2024-09-03 10:58:23 -0300

    first commit

---
+++ src/main/java/br/thullyo/springsecurity6jwt/SpringSecurity6JwtApplication.java
@@ -0,0 +1,13 @@
+package br.thullyo.springsecurity6jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyo/springsecurity6jwt/SpringSecurity6JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyo.springsecurity6jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
