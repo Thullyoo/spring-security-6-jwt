706fa1bafb38efa051adb8ed9a3711786ef0d12e
Thullyoo <thullyofilho@gmail.com>
2024-09-03 11:02:57 -0300

    feat: criação da principal entity

---
+++ src/main/java/br/thullyo/springsecurity6jwt/entity/User.java
@@ -0,0 +1,29 @@
+package br.thullyo.springsecurity6jwt.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "TB_USERS")
+public class User {
+
+    @Id
+    private String name;
+
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
